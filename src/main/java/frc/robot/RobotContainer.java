11614ff16a658fce86e55b724abc930749806738
Melissa1792 <mintdameow@gmail.com>
2026-02-17 21:09:57 -0600

    changed shooter RPS

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,7 @@ import frc.robot.subsystems.Turret.TurretState;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(1).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -201,7 +201,7 @@ public class RobotContainer {
             .onFalse(indexer.runOnce(() -> indexer.setGoal(IndexerState.STOP)));
         
         joystick.rightTrigger()
-            .onTrue(shooter.runOnce(() -> shooter.setShooterVelocity(20)))
+            .onTrue(shooter.runOnce(() -> shooter.setShooterVelocity(60)))
             .onFalse(shooter.runOnce(() -> shooter.shooterOff()));
 
         joystick.povUp()
